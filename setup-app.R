cef0c1b7c0ec1a292517efa5b8390464923fc791
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2022-02-18 08:24:47 +0100

    added setup

---
+++ setup-app.R
@@ -0,0 +1,7 @@
+is_renv <- try(find.package("renv"), silent = TRUE)
+if(inherits(is_renv, "try-error")) {
+  install.packages("renv")
+}
+renv::restore(lockfile = "https://raw.githubusercontent.com/michbur/easyR/main/renv.lock")
+download.file("https://raw.githubusercontent.com/michbur/easyR/main/app.R", destfile = "app.R")
+shiny::runApp()
