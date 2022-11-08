5f806068e79e2279a2fd8e55ca270c98e123d0ba
KrystynaGrzesiak <krygrz11@gmail.com>
2022-11-08 15:43:53 +0100

    głupie rzeczy

--- R/gui.R
+++ R/gui.R
@@ -1,14 +1,4 @@
-#' easyR Graphical User Interface
-#'
-#' @description Launches graphical user interface.
-#' 
-#' @importFrom shiny runApp
-#' 
-#' @param port The TCP port. See \code{\link[shiny]{runApp}}.
-#' 
-#' @section Warning : Any ad-blocking software may cause malfunctions.
-#' 
-#' @export run_MetaboModelleR
+
 
 run_MetaboModelleR <- function(port = getOption("shiny.port"))
   runApp(system.file("app", package = "MetaboModelleR"), port = port, launch.browser = TRUE)
